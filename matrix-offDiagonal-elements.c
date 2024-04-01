836394ea428935d52145605ec252a198d5e676df
AADITHYA V ANAND <aadithyadreams@gmail.com>
2024-04-01 23:25:33 +0530

    Create matrix-offDiagonal-elements.c

---
+++ matrix-offDiagonal-elements.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main(){
+    int a[10][10],i,j,m,n;
+    printf("limits:");
+    scanf("%d%d",&m,&n);
+    printf("elements:\n");
+    for(i=0;i<m;i++){
+        for(j=0;j<n;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+    printf("matrix:\n");
+    for(i=0;i<m;i++){
+        for(j=0;j<n;j++){
+            if(i+j==n-1){
+                printf("%d ",a[i][j]);
+            }
+            else{
+                printf("  ");
+            }
+        }
+        printf("\n");
+    }
+}
